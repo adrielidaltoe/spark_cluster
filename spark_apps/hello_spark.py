a02e79609d824f9fbe6bfb4c6d790aa7a0eb01f4
adrieli.daltoe@gmail.com <adrieli.daltoe@gmail.com>
2024-07-15 06:43:44 -0300

    code modularization

--- spark_apps/hello_spark.py
+++ spark_apps/hello_spark.py
@@ -1,19 +1,31 @@
 from pyspark.sql import *
 
 from lib.logger import Log4J
-from lib.utils import get_spark_app_config
+from lib.utils import *
 
 if __name__ == "__main__":
 
     conf = get_spark_app_config()
 
-    spark = SparkSession.builder \
+    spark = SparkSession \
+        .builder \
         .config(conf=conf) \
         .getOrCreate()
     
     logger = Log4J(spark)
 
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
     logger.info('Starting program')
+
+    survey_raw_df = load_survey_df(spark, sys.argv[1])
+    partitioned_survey_df = survey_raw_df.repartition(2)
+    count_df = count_by_country(partitioned_survey_df)
+    count_df.show()
+
+    input("Press Enter")
 
     logger.info('Finishing program')
 
--- spark_apps/lib/utils.py
+++ spark_apps/lib/utils.py
@@ -1,4 +1,4 @@
-'''import configparser
+import configparser
 
 from pyspark import SparkConf
 
@@ -7,18 +7,22 @@ def get_spark_app_config():
     spark_conf = SparkConf()
     config = configparser.ConfigParser()
     config.read('spark.conf')
-    print(config)
 
     for key, value in config.items('SPARK_APP_CONFIGS'):
         spark_conf.set(key, value)
 
     return spark_conf
-'''
-
-import configparser
 
 
-def get_spark_app_config():
-    config = configparser.ConfigParser()
-    config.read('spark.conf')
-    print(config)
+def load_survey_df(spark, data_file):
+    return spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv(data_file)
+
+
+def count_by_country(survey_df):
+    return survey_df.filter("Age < 40") \
+        .select("Age", "Gender", "Country", "state") \
+        .groupBy("Country") \
+        .count()
